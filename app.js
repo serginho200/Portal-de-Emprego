10a1cbb3f2b82725c469a3bbdbc41d87d9376bac
sergiocarvalho290@gmail.com <sergiocarvalho290@gmail.com>
2024-04-24 11:07:33 -0300

    primeiro commit

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const exphbs = require('express-handlebars');
+const app = express();
+const path = require('path');
+const db = require('./db/connection.js');
+const bodyParser = require('body-parser');
+
+const PORT = 3000;
+
+app.listen(PORT, function () {
+  console.log(`O Express está na porta ${PORT}`);
+});
+
+//body parser
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// hadle bars
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', exphbs.engine({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+// static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+//db connection
+db.authenticate()
+  .then(() => {
+    console.log("Conectou ao banco com sucesso!");
+  })
+  .catch((err) => {
+    console.log("Ocorreu um erro ao conectar! ", err);
+  });
+
+//routes
+app.get("/", (req, res) => {
+  res.render('index');
+});
+
+// jobs routes
+app.use("/jobs", require("./routes/jobs"));
